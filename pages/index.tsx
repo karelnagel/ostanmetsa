fc37829b3ecf974d76eb1f865f4344351e659f89
Karel <karel789@hotmail.com>
2022-03-21 19:51:10 +0200

    added contact form

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,33 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { Post } from "../interfaces";
+import Link from "next/link";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Home({ posts }: { posts: Post[] }) {
+  const [sending,setSending ] =useState(false)
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = async (e: any) => {
+    e.preventDefault();
+    console.log("sending")
+    if (sending) return;
+    setSending(true);
+    try {
+      const result = await emailjs.sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID!,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID!,
+        form.current!,
+        process.env.NEXT_PUBLIC_USER_ID
+      );
+      console.log(result.text);
+      form.current?.reset();
+    } catch (e) {
+      console.log(e);
+    }
+    setSending(false);
+  };
   return (
     <main>
       <nav className={styles.nav}>
@@ -34,7 +59,7 @@ export default function Home({ posts }: { posts: Post[] }) {
           {teenused.map((teenus, i) => (
             <a key={i} className={styles.teenus} href={"/"}>
               <Image className={styles.teenusedImage} src={teenus.image} alt={teenus.title}></Image>
-              <p className={styles.title}>{teenus.title}</p>
+              <h3 className={styles.title}>{teenus.title}</h3>
               <p className={styles.description}>{teenus.description}</p>
             </a>
           ))}
@@ -42,15 +67,35 @@ export default function Home({ posts }: { posts: Post[] }) {
       </div>
 
       <div className={styles.blog} id="blogi">
-        <h2>Uudised</h2>
-        {posts.map((post: any, i: number) => (
-          <a key={i} href={`/blog/${post.slug}`}>
-            <h2>{post.title}</h2>
-            <Image src={post.thumbnailUrl} alt={post.title} width="100" height="100"></Image>
-            <p>{post.description}</p>
-          </a>
-        ))}
+        <h2>Viimased uudised</h2>
+        <div>
+          {posts.slice(0, 3).map((post: any, i: number) => (
+            <a key={i} href={`/blog/${post.slug}`} className={styles.post}>
+              <Image src={post.thumbnailUrl} alt={post.title} layout="fill" className={styles.postImage}></Image>
+              <div className={styles.postText}>
+                <h3>{post.title}</h3>
+              </div>
+            </a>
+          ))}
+        </div>
+        <Link href="/blog">
+          <a className={styles.blogButton}>Loe rohkem</a>
+        </Link>
       </div>
+
+      <div id="kontakt" className={styles.contact}>
+        <h2 className={styles.sectionTitle}>Kontakt</h2>
+
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" placeholder="Nimi" name="name" />
+          <input type="mail" placeholder="E-post" name="email" required />
+          <textarea placeholder="Sõnum" name="message" required></textarea>
+          <button className="button" type="submit">
+            {sending ? "Saadan ..." : "Saada"}
+          </button>
+        </form>
+      </div>
+
       <footer className={styles.footer}></footer>
     </main>
   );
